4bb9cd50abf3711fd39e69e3b9d6c11a64de39d0
Isaac Orme <isorme1@gmail.com>
2017-10-31 13:15:04 -0700

    slide-in-images

---
+++ scripts/main.js
@@ -0,0 +1,31 @@
+const images = document.querySelectorAll('.slide-in');
+
+function checkSlide(e) {
+  images.forEach( img =>{
+    // half way through the image
+    const slideInAt = (window.scrollY + window.innerHeight) - (img.height / 2);
+    // bottom of the image
+    const imageBottom = img.offsetTop + img.height;
+    const isHalfShown = slideInAt > img.offsetTop;
+    const isNotScrolledPast = window.scrollY < imageBottom;
+
+    isHalfShown && isNotScrolledPast ? img.classList.add("active") : img.classList.remove("active");
+  });
+}
+
+window.addEventListener('scroll', debounce(checkSlide));
+
+function debounce(func, wait = 20, immediate = true) {
+  var timeout;
+  return function() {
+    var context = this, args = arguments;
+    var later = function() {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    var callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+}
